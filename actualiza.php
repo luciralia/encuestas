2bd4be5831c527221fe12daf67b6b09ed23e080c
Lux <luciraliahdez@yahoo.com>
2018-11-09 16:15:23 -0600

    con documento actualiza

---
+++ actualiza.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+</head>
+<?php echo 'Modificando y agregando funcionalidad'; ?>
+<body>
+</body>
+</html>
